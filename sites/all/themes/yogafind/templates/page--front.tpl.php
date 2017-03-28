d6d88d9ed2d99761d05c36dc3a832d8a6655732b
Owen Williams <owen@zoocha.com>
2017-03-28 20:28:21 +0000

    updates

--- sites/all/themes/yogafind/templates/page--front.tpl.php
+++ sites/all/themes/yogafind/templates/page--front.tpl.php
@@ -56,17 +56,19 @@
     <div class="hero-content container">
       <div class="hero left animated fadeInUp">
         <h1>Yoga Find</br>the easiest way to find yoga in the UK</h1>
-        <h2>We aim to provide the easiest way to find studios, instructors, yoga events and yoga classes all over the UK.</h2>
-        <p><strong style="font-size: 27px;">We've just launched</strong> It's free to get started - so create your yoga listings now!.</p>
+        <h2>We aim to provide the easiest way to find studios, instructors, yoga
+          events and yoga classes all over the UK.</h2>
+        <p><strong style="font-size: 27px;">We've just launched</strong> It's
+          free to get started - so create your yoga listings now!.</p>
       </div>
       <div class="hero right">
         <?php
-//          $slick_block = block_load('webform', 'client-block-166');
-//          $block = _block_get_renderable_array(_block_render_blocks(array($slick_block)));
-//          print drupal_render($block);
-          $slick_block = block_load('models_searcher', 'main_searcher');
-          $block = _block_get_renderable_array(_block_render_blocks(array($slick_block)));
-          print drupal_render($block);
+        //          $slick_block = block_load('webform', 'client-block-166');
+        //          $block = _block_get_renderable_array(_block_render_blocks(array($slick_block)));
+        //          print drupal_render($block);
+        $slick_block = block_load('models_searcher', 'main_searcher');
+        $block = _block_get_renderable_array(_block_render_blocks(array($slick_block)));
+        print drupal_render($block);
         ?>
       </div>
     </div>
@@ -75,23 +77,40 @@
   <?php print $messages; ?>
 
   <div id="yf-sign-up" class="container">
-    Looking to list your yoga classes, event or studio? Join ### others and <a href="/getting-started" class="a-link link-go">get started now <i class="material-icons">trending_flat</i></a>
+    Looking to list your yoga classes, event or studio? Join ### others and <a
+      href="/getting-started" class="a-link link-go">get started now <i
+        class="material-icons">trending_flat</i></a>
   </div>
 
   <div id="yf-events" class="container">
-    <h3 class="text-center hd-pink">YogaFind Events</h3>
-    <h4>On your mat, get set, yoga!</h4>
-    --- event listings here using current styles ---
+    <div class="col-md-6">
+      <h3 class="hd-purple">Upcoming Events</h3>
+      <h4>On your mat, get set, yoga!</h4>
+      <?php
+      $slick_block = block_load('views', 'yoga_event_lists-block_2');
+      $block = _block_get_renderable_array(_block_render_blocks(array($slick_block)));
+      print drupal_render($block);
+      ?>
+    </div>
+    <div class="col-md-6">
+      <h3 class="hd-purple">Latest Teachers</h3>
+      <h4>Check out our page!</h4>
+      <?php
+      $slick_block = block_load('views', 'yoga_searcher-block_2');
+      $block = _block_get_renderable_array(_block_render_blocks(array($slick_block)));
+      print drupal_render($block);
+      ?>
+    </div>
   </div>
 
   <div id="yf-events" class="container">
-    <h3 class="text-center hd-pink">YogaFind Posts</h3>
+    <h3 class="text-center hd-purple">YogaFind Posts</h3>
     <h4>Check out these latest posts!</h4>
     --- event posts here using current styles ---
   </div>
 
   <div id="yf-listings" class="container">
-    <h3 class="text-center hd-pink">Popular places</h3>
+    <h3 class="text-center hd-purple">Popular places</h3>
     <ul>
       <li>London</li>
       <li>England</li>
@@ -102,54 +121,70 @@
   </div>
 
   <div id="what-is-it" class="container">
-    <h3 class="text-center hd-pink">Why YogaFind?</h3>
+    <h3 class="text-center hd-purple">Why YogaFind?</h3>
     <div class="col-md-3">
       <div class="info-img">
-        <img src="/sites/all/themes/models/images/whyhbm.jpg" class="img-responsive" title="Why HBM info image"/>
+        <img src="/sites/all/themes/models/images/whyhbm.jpg"
+             class="img-responsive" title="Why HBM info image"/>
       </div>
       <div class="info-surround">
         <h2>Why Hair and Beauty Models?</h2>
-        <p>Hair and Beauty Models <strong>#HBM</strong> is here to help hair dressers and beauticians, trainees or professionals connect with reliable last minute models for assessments, client work or offers. Interested in joining? <a href="/user/register" class="a-link">Sign up</a> now! It's <strong>free!</strong></p>
+        <p>Hair and Beauty Models <strong>#HBM</strong> is here to help hair
+          dressers and beauticians, trainees or professionals connect with
+          reliable last minute models for assessments, client work or offers.
+          Interested in joining? <a href="/user/register" class="a-link">Sign
+            up</a> now! It's <strong>free!</strong></p>
       </div>
     </div>
     <div class="col-md-3">
       <div class="info-img">
-        <img src="/sites/all/themes/models/images/but.jpg" class="img-responsive" title="But I'm not a .. info image"/>
+        <img src="/sites/all/themes/models/images/but.jpg"
+             class="img-responsive" title="But I'm not a .. info image"/>
       </div>
       <div class="info-surround">
         <h2>But I'm not a hair dresser or beautician?</h2>
-        <p>We've got you covered! <a href="/user/register" class="a-link">Sign up</a> as a Last Minute Model, search the available jobs and request a treatment. Nothing you fancy, but looking for a job? Post a <strong>last minute model</strong> job and let the professionals come to you!</p>
+        <p>We've got you covered! <a href="/user/register" class="a-link">Sign
+            up</a> as a Last Minute Model, search the available jobs and request
+          a treatment. Nothing you fancy, but looking for a job? Post a <strong>last
+            minute model</strong> job and let the professionals come to you!</p>
       </div>
     </div>
     <div class="col-md-3">
       <div class="info-img">
-        <img src="/sites/all/themes/models/images/noshow.jpg" class="img-responsive" title="No more no shows! info image"/>
+        <img src="/sites/all/themes/models/images/noshow.jpg"
+             class="img-responsive" title="No more no shows! info image"/>
       </div>
       <div class="info-surround">
         <h2>No more no shows!</h2>
-        <p>At HBM we understand your frustration when a client doesn't show up.  With HBM you can <strong>leave feedback</strong> and <strong>give a 5 star rating</strong> against the client and owner, to enable more informed selections on future jobs.</p>
+        <p>At HBM we understand your frustration when a client doesn't show up.
+          With HBM you can <strong>leave feedback</strong> and <strong>give a 5
+            star rating</strong> against the client and owner, to enable more
+          informed selections on future jobs.</p>
       </div>
     </div>
     <div class="col-md-3">
       <div class="info-img">
-        <img src="/sites/all/themes/models/images/what.jpg" class="img-responsive" title="what about my business. info image"/>
+        <img src="/sites/all/themes/models/images/what.jpg"
+             class="img-responsive" title="what about my business. info image"/>
       </div>
       <div class="info-surround">
         <h2>What about my business?</h2>
-        <p>It's <strong>free</strong> to sign up - so create your profile and link to your website for more traffic. Have a special offer coming up? Post a quick job and get your business out there!</p>
+        <p>It's <strong>free</strong> to sign up - so create your profile and
+          link to your website for more traffic. Have a special offer coming up?
+          Post a quick job and get your business out there!</p>
       </div>
     </div>
   </div>
 
-    <div id="what-we-offer" class="clearfix container">
-      <h3 class="text-center hd-pink">What we offer..</h3>
-      <div class="row">
+  <div id="what-we-offer" class="clearfix container">
+    <h3 class="text-center hd-purple">What we offer..</h3>
+    <div class="row">
       <div class="col-md-4">
         <div class="what-block">
           <h3>Create up to 5 HBM active jobs</h3>
-         <!--  <div class="search-img">
-            <img class="img-responsive" src="/sites/all/themes/models/images/search-example.png" alt="Search for jobs" title="Search for jobs"/>
-          </div> -->
+          <!--  <div class="search-img">
+             <img class="img-responsive" src="/sites/all/themes/models/images/search-example.png" alt="Search for jobs" title="Search for jobs"/>
+           </div> -->
         </div>
       </div>
       <div class="col-md-4">
@@ -174,9 +209,9 @@
       <div class="col-md-4">
         <div class="what-block">
           <h3>Create a profile and link to your business</h3>
-         <!--  <div class="search-img">
-            <img class="img-responsive" src="/sites/all/themes/models/images/search-example.png" alt="Search for jobs" title="Search for jobs"/>
-          </div> -->
+          <!--  <div class="search-img">
+             <img class="img-responsive" src="/sites/all/themes/models/images/search-example.png" alt="Search for jobs" title="Search for jobs"/>
+           </div> -->
         </div>
       </div>
       <div class="col-md-4">
@@ -201,14 +236,19 @@
   <div id="look-and-feel" class="clearfix">
     <div class="lhs pull-left">
       <div class="what-block">
-        <h3 class="text-center hd-pink">Mobile Friendly</h3>
-        <p>Hair and Beauty Models has been optimised for mobile to allow you to access your jobs, requests and offers in the most efficient way. What are you waiting for? Check it out on mobile <a href="/user/register" class="a-link">Sign up</a> now!</p>
+        <h3 class="text-center hd-purple">Mobile Friendly</h3>
+        <p>Hair and Beauty Models has been optimised for mobile to allow you to
+          access your jobs, requests and offers in the most efficient way. What
+          are you waiting for? Check it out on mobile <a href="/user/register"
+                                                         class="a-link">Sign
+            up</a> now!</p>
       </div>
     </div>
     <div class="rhs pull-right">
       <div class="what-block">
         <div class="mobile">
-          <img src="/sites/all/themes/models/images/mobile-view.png" class="img-responsive"/>
+          <img src="/sites/all/themes/models/images/mobile-view.png"
+               class="img-responsive"/>
         </div>
       </div>
     </div>
@@ -217,10 +257,11 @@
 </main>
 
 <div class="main-container <?php print $container_class; ?>">
-   <div class="row">
+  <div class="row">
     <section<?php print $content_column_class; ?>>
       <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+        <div
+          class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
       <?php if (!empty($page['help'])): ?>
         <?php print render($page['help']); ?>
@@ -232,7 +273,7 @@
         <?php print render($page['search_top']); ?>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php  if (isset($client_request_confirm_form)) : print $client_request_confirm_form; endif; ?>
+      <?php if (isset($client_request_confirm_form)) : print $client_request_confirm_form; endif; ?>
     </section>
   </div>
 </div>
@@ -246,7 +287,8 @@
       </footer>
     <?php endif; ?>
     <footer class="footer-cr container-fluid">
-      <span class="cr"><?php print '&copy; ' . t(':date hairandbeautymodels.com all rights reserved.', array(':date' => date('Y', strtotime('now')))); ?></span>
+      <span
+        class="cr"><?php print '&copy; ' . t(':date hairandbeautymodels.com all rights reserved.', array(':date' => date('Y', strtotime('now')))); ?></span>
     </footer>
   </div>
 <?php endif; ?>

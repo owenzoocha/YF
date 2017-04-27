b54589991d5ec59b6dcfced07fca9a976dbaf85a
Owen Williams <owen@zoocha.com>
2017-04-27 21:03:26 +0000

    front page tweaks etc

--- sites/all/modules/custom/models_forms/templates/login-form.tpl.php
+++ sites/all/modules/custom/models_forms/templates/login-form.tpl.php
@@ -16,29 +16,11 @@
   </div>
 </div>
 
-<!--<div class="row">-->
-<!--  <div class="col-sm-2">-->
-<!--    <label class="control-label" for="edit-field-my-web-und-0-url">Web: </label>-->
-<!--  </div>-->
-<!--    <div class="col-sm-8">-->
-<!--    --><?php //print //render($form['field_my_web']); ?>
-<!--  </div>-->
-<!--</div>-->
 <div class="row">
   <div class="col-sm-12">
     <h2>Profile picture</h2>
-    <?php print render($form['picture']); ?>
-  </div>
-<!--  <div class="col-sm-12">-->
-<!--    --><?php ////print render($form['field_hb_cover_pic']); ?>
-<!--  </div>-->
+  <?php print render($form['picture']); ?>
 </div>
-<!--<div class="row">-->
-<!--  <div class="col-sm-12">-->
-<!--    <h2>Address</h2>-->
-<!--    --><?php ////print render($form['geocomplete']); ?>
-<!--    --><?php ////print render($form['field_my_address']); ?>
-<!--  </div>-->
-<!--</div>-->
+</div>
 
 <?php print drupal_render_children($form) ?>
